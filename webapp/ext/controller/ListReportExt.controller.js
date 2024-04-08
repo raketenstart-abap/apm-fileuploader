353f60ec70de356e552cc642385c02300db8ee77
Arthur Silva <arthur.silva@fuscella.com>
2024-04-08 18:05:38 +0100

    feat(clas): enable file import

--- webapp/ext/controller/ListReportExt.controller.js
+++ webapp/ext/controller/ListReportExt.controller.js
@@ -1,21 +1,28 @@
 sap.ui.define([
   "sap/ui/core/Fragment",
-  "sap/m/MessageBox"
-], function (Fragment, MessageBox) {
+  "sap/ui/unified/FileUploaderParameter",
+  "sap/m/MessageBox",
+  "sap/m/MessageToast"
+], function (Fragment, FileUploaderParameter, MessageBox, MessageToast) {
   "use strict";
 
   function _createUploadController(controller) {
     var oUploadDialog;
 
-    /**
-     * 
-     */
+    function setDialogBusy(state) {
+      oUploadDialog.setBusy(state);
+    }
+
     function closeDialog() {
       oUploadDialog.close();
     }
 
     function showError(message) {
       MessageBox.error(message || "Upload failed");
+    }
+
+    function byId(id) {
+      return Fragment.byId("uploadFileDialog", id);
     }
 
     return {
@@ -30,6 +37,36 @@ sap.ui.define([
         view.removeDependent(oUploadDialog);
         oUploadDialog.destroy();
         oUploadDialog = undefined;
+      },
+
+      onImport: function () {
+        setDialogBusy(true);
+
+        const fileUploader = byId("uploadFileDialog_uploader");
+        const csrfToken = controller.getView().getModel().getSecurityToken();
+
+        var headerParma = new FileUploaderParameter();
+        headerParma.setName("x-csrf-token");
+        headerParma.setValue(csrfToken);
+        fileUploader.addHeaderParameter(headerParma);
+
+        var headerParma2 = new FileUploaderParameter();
+        headerParma2.setName("slug");
+        headerParma2.setValue(fileUploader.getValue());
+        fileUploader.addHeaderParameter(headerParma2);
+
+        fileUploader.upload();
+        fileUploader.destroyHeaderParameters();
+
+        // fileUploader
+        //   .checkFileReadable()
+        //   .then(function () {
+        //     fileUploader.upload();
+        //   })
+        //   .catch(function () {
+        //     showError("The file cannot be read.");
+        //     setDialogBusy(false);
+        //   });
       },
 
       onCancel: function () {
@@ -51,8 +88,31 @@ sap.ui.define([
           + " is not supported. Choone one of the following types: "
           + supportedTypes
         );
-      }
+      },
 
+      onUploadComplete: function (oEvent) {
+        const status = oEvent.getParameter("status");
+        const fileUploader = oEvent.getSource();
+
+        fileUploader.clear();
+        setDialogBusy(false);
+
+        switch (status) {
+          case 415:
+            showError(oEvent.getParameter("response"));
+            break;
+
+          default:
+            if (status >= 400) {
+              const responseRaw = JSON.parse(oEvent.getParameter("responseRaw"));
+              showError(responseRaw && responseRaw.error && responseRaw.error.message);
+            } else {
+              MessageToast.show("Upload successfully");
+              controller.extensionAPI.refreshTable();
+              closeDialog();
+            }
+        }
+      }
     };
 
   }
@@ -60,7 +120,7 @@ sap.ui.define([
   return {
     upload: function () {
       Fragment.load({
-        id: "uploadDialog",
+        id: "uploadFileDialog",
         name: "apm.fileuploader.ext.fragments.popup.UploadDialog",
         controller: _createUploadController(this)
       }).then(function (oDialog) {
